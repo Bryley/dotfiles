7a52f123249f07ccf603d2d89d9061ff516ef555
Bryley Hayter <bryley@pop-os.localdomain>
2021-10-24 19:46:59 +1000

    Added a few nvim plugins and customistations

--- nvim/pluginconfigs/lspsetup.lua
+++ nvim/pluginconfigs/lspsetup.lua
@@ -1,6 +1,8 @@
 local lsp_installer = require("nvim-lsp-installer")
 local coq = require "coq"
 
+local map = vim.api.nvim_set_keymap
+
 lsp_installer.on_server_ready(function(server)
     local opts = {coq.lsp_ensure_capabilities()}
 
@@ -13,3 +15,11 @@ lsp_installer.on_server_ready(function(server)
     server:setup(opts)
     vim.cmd [[ do User LspAttachBuffers ]]
 end)
+
+
+-- LSP saga --
+
+-- Show line diagnostics with <Space>cc
+map('n', '<leader>cc',
+'<cmd>lua require\'lspsaga.diagnostic\'.show_cursor_diagnostics()<CR>',
+{noremap = true, silent = true})
