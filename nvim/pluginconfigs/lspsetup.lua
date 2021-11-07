975233fb9a229c9010baced4fd1e788a3022bcae
Bryley Hayter <bryley@pop-os.localdomain>
2021-11-08 09:17:52 +1000

    Added python path to bottom bar. - Toyed around with lspsaga but had issues like code_action not working

--- nvim/pluginconfigs/lspsetup.lua
+++ nvim/pluginconfigs/lspsetup.lua
@@ -1,5 +1,6 @@
 local lsp_installer = require("nvim-lsp-installer")
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+--local saga = require('lspsaga')
 
 local map = vim.api.nvim_set_keymap
 
@@ -26,14 +27,18 @@ lsp_installer.on_server_ready(function(server)
     vim.cmd [[ do User LspAttachBuffers ]]
 end)
 
-map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap=true, silent=true })
-map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', { noremap=true, silent=true })
-map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap=true, silent=true })
-map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', { noremap=true, silent=true })
-map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap=true, silent=true })
-map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { noremap=true, silent=true })
-map('n', '<C-n>', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', { noremap=true, silent=true })
-map('n', '<C-p>', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', { noremap=true, silent=true })
+--saga.init_lsp_saga()
+
+--map('n', 'gd', "<cmd>lua lua require('lspsaga.hover').render_hover_doc()<CR>", { noremap=true, silent=true })
+
+map('n', 'gd'	, '<cmd>lua vim.lsp.buf.definition()<CR>',		{ noremap=true, silent=true })
+map('n', 'gD'	, '<cmd>lua vim.lsp.buf.declaration()<CR>',		{ noremap=true, silent=true })
+map('n', 'gr'	, '<cmd>lua vim.lsp.buf.references()<CR>',		{ noremap=true, silent=true })
+map('n', 'gi'	, '<cmd>lua vim.lsp.buf.implementation()<CR>',	{ noremap=true, silent=true })
+map('n', 'K'	, '<cmd>lua vim.lsp.buf.hover()<CR>',			{ noremap=true, silent=true })
+map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>',	{ noremap=true, silent=true })
+map('n', '<C-n>', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>',{ noremap=true, silent=true })
+map('n', '<C-p>', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',{ noremap=true, silent=true })
 
 
 -- LSP saga --
--- nvim/pluginconfigs/lualine.lua
+++ nvim/pluginconfigs/lualine.lua
@@ -1,3 +1,19 @@
+
+-- Returns the virtual env that is currently activated with python
+local function virtualenv()
+	local shell = io.popen("which python3")
+	local result = shell:read()
+	shell:close()
+
+	return result
+end
+
+
 require'lualine'.setup({
-	options = {theme = 'auto'}
+	options = {
+		theme = 'auto'
+	},
+	sections = {
+		lualine_c = {'filename', virtualenv}
+	}
 })
