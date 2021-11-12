0567c9cfcfa96d3341b6ac2cb077bd2a6933d8d7
bryley.manninghayter <bryley.manninghayter@entaingroup.com.au>
2021-11-12 13:34:25 +1000

    Added surround.vim and repeat.vim plugins to neovim.

--- nvim/pluginconfigs/telescope.lua
+++ nvim/pluginconfigs/telescope.lua
@@ -5,3 +5,13 @@ map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', opts)
 map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', opts)
 map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', opts)
 map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', opts)
+
+local telescope = require('telescope')
+
+telescope.setup {
+    pickers = {
+        find_files = {
+            hidden = true
+        }
+    }
+}
