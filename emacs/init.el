2482aaf58dcba6fa2d52425d45ad00a62c8fe66d
Bryley Hayter <bryley.manninghayter@ladbrokes.com.au>
2022-05-09 17:08:45 +1000

    More emacs additions

--- emacs/init.el
+++ emacs/init.el
@@ -9,8 +9,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("6c4c97a17fc7b6c8127df77252b2d694b74e917bab167e7d3b53c769a6abb6d6" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "0d01e1e300fcafa34ba35d5cf0a21b3b23bc4053d388e352ae6a901994597ab1" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" default))
+ '(org-agenda-files nil)
  '(package-selected-packages
-   '(magit company-box company consult-lsp centaur-tabs vterm-toggle popper vterm pyvenv undo-tree eyebrowse evil-surround evil-commentary marginalia vertico consult treemacs-icons-dired treemacs-projectile treemacs-evil treemacs neotree dashboard sublimity helpful lsp-treemacs lsp-ui lsp-pyright counsel-projectile projectile counsel rainbow-delimiters scroll-on-jump which-key doom-modeline org-bullets doom-themes general evil-collection evil use-package)))
+   '(vscode-dark-plus-theme numpydoc yasnippet auto-virtualenv tree-sitter-langs tree-sitter treesitter-lang treesitter magit company-box company consult-lsp centaur-tabs vterm-toggle popper vterm pyvenv undo-tree eyebrowse evil-surround evil-commentary marginalia vertico consult treemacs-icons-dired treemacs-projectile treemacs-evil treemacs neotree dashboard sublimity helpful lsp-treemacs lsp-ui lsp-pyright counsel-projectile projectile counsel rainbow-delimiters scroll-on-jump which-key doom-modeline org-bullets doom-themes general evil-collection evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
