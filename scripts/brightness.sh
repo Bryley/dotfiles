80a15bba2561a4bb136661fdd65e810d5b66dfad
Bryley <bryleyhayter@gmail.com>
2022-05-10 22:20:33 +1000

    Updated brightness and volume scripts

--- scripts/brightness.sh
+++ scripts/brightness.sh
@@ -8,18 +8,33 @@ case $1 in
         awk -v c=$CURRENT_BRIGHTNESS -v m=$MAX_BRIGHTNESS 'BEGIN {print (c/m)*100 }'
         ;;
     set)
-        awk -v m=$MAX_BRIGHTNESS -v n=$2 'BEGIN {print ( (n/100) * m )}' > $BACKLIGHT_DIR/brightness
+        BRIGHTNESS=$(awk -v m=$MAX_BRIGHTNESS -v n=$2 'BEGIN {print ( (n/100) * m )}')
+        BRIGHTNESS=$(printf "%.0f\n" "$BRIGHTNESS")
+        
+        echo $BRIGHTNESS > $BACKLIGHT_DIR/brightness
+        # Return new percentage
+        echo $2
         ;;
     up)
-        NEW_PERC=$(awk -v c=$CURRENT_BRIGHTNESS -v m=$MAX_BRIGHTNESS 'BEGIN {print (c/m)*100 + 5 }')
-        awk -v m=$MAX_BRIGHTNESS -v n=$NEW_PERC 'BEGIN {print ( (n/100) * m )}' > $BACKLIGHT_DIR/brightness
-        ;;
+        NEW_PERC=$(awk -v c=$CURRENT_BRIGHTNESS -v m=$MAX_BRIGHTNESS 'BEGIN { print (c/m) * 100 + 5 }')
+        NEW_PERC=$(printf "%.0f\n" "$NEW_PERC")
+        BRIGHTNESS=$(awk -v m=$MAX_BRIGHTNESS -v n=$NEW_PERC 'BEGIN {print ( (n/100) * m )}')
+        BRIGHTNESS=$(printf "%.0f\n" "$BRIGHTNESS")
 
+        echo $BRIGHTNESS > $BACKLIGHT_DIR/brightness
+        # Return new percentage
+        echo $NEW_PERC
+        ;;
     down)
-        NEW_PERC=$(awk -v c=$CURRENT_BRIGHTNESS -v m=$MAX_BRIGHTNESS 'BEGIN {print (c/m)*100 - 5 }')
-        awk -v m=$MAX_BRIGHTNESS -v n=$NEW_PERC 'BEGIN {print ( (n/100) * m )}' > $BACKLIGHT_DIR/brightness
-        ;;
+        NEW_PERC=$(awk -v c=$CURRENT_BRIGHTNESS -v m=$MAX_BRIGHTNESS 'BEGIN { print (c/m) * 100 - 5 }')
+        NEW_PERC=$(printf "%.0f\n" "$NEW_PERC")
+        BRIGHTNESS=$(awk -v m=$MAX_BRIGHTNESS -v n=$NEW_PERC 'BEGIN {print ( (n/100) * m )}')
+        BRIGHTNESS=$(printf "%.0f\n" "$BRIGHTNESS")
 
+        echo $BRIGHTNESS > $BACKLIGHT_DIR/brightness
+        # Return new percentage
+        echo $NEW_PERC
+        ;;
     *)
         echo "Don't know what that means"
         ;;
--- scripts/volume.sh
+++ scripts/volume.sh
@@ -3,23 +3,21 @@
 
 case $1 in
     get)
-        amixer sget Master | awk -F '[][]' '/Left:/ {print $2}'
+        # amixer sget Master | awk -F '[][]' '/Left:/ {print $2}'
+        amixer sget Master | awk -F '[][]' '{print $2}' | awk '/[0-9]/ { print $0 }'
         ;;
-
     set)
-        amixer set Master $2 | awk -F '[][]' '/Left:/ {print $2}'
+        amixer set Master $2 | awk -F '[][]' '{print $2}' | awk '/[0-9]/ { print $0 }'
         ;;
     mute)
-        amixer set Master toggle | awk -F '[][]' '/Left:/ {print $2}'
+        amixer set Master toggle | awk -F '[][]' '{print $2}' | awk '/[0-9]/ { print $0 }'
         ;;
     up)
-        amixer set Master 5%+ | awk -F '[][]' '/Left:/ {print $2}'
+        amixer set Master 5%+ | awk -F '[][]' '{print $2}' | awk '/[0-9]/ { print $0 }'
         ;;
-
     down)
-        amixer set Master 5%- | awk -F '[][]' '/Left:/ {print $2}'
+        amixer set Master 5%- | awk -F '[][]' '{print $2}' | awk '/[0-9]/ { print $0 }'
         ;;
-
     *)
         echo "Don't know what that means"
         ;;
