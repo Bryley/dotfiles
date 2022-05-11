ee0fb44cc9bbc67cedc5808ca5f72af656810d7b
Bryley <bryley.manninghayter@ladbrokes.com.au>
2022-05-12 09:40:26 +1000

    Setup ssh-agent to startx

--- startx.sh
+++ startx.sh
@@ -1,2 +1,4 @@
 # This is so that alacritty won't crash if window is too large
-startx -- -maxbigreqsize 127
+ssh-agent startx -- -maxbigreqsize 127
+
+# The 'ssh-agent' is used to manage multiple ssh-keys easily
