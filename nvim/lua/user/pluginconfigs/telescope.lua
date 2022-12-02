7b43923a7662c64dfba1a166b884ba5803d231e1
Bryley <bryley.manninghayter@ladbrokes.com.au>
2022-12-02 17:41:01 +1000

    Added pnpm config to zsh

--- nvim/lua/user/pluginconfigs/telescope.lua
+++ nvim/lua/user/pluginconfigs/telescope.lua
@@ -1,21 +1,30 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
+	return
+end
+
+local status_ok2, trouble = pcall(require, "trouble.providers.telescope")
+if not status_ok2 then
     return
 end
 
-local actions = require "telescope.actions"
+local actions = require("telescope.actions")
 
-telescope.setup {
-    pickers = {
-        find_files = {
-            hidden = true,
-            no_ignore = true,
-        }
-    },
-    defaults = {
-        file_ignore_patterns = {
-            ".venv",
-            "node_modules"
-        }
-    }
-}
+telescope.setup({
+	pickers = {
+		find_files = {
+			hidden = true,
+			no_ignore = true,
+		},
+	},
+	defaults = {
+		file_ignore_patterns = {
+			".venv",
+			"node_modules",
+		},
+		mappings = {
+			i = { ["<c-t>"] = trouble.open_with_trouble },
+			n = { ["<c-t>"] = trouble.open_with_trouble },
+		},
+	},
+})
