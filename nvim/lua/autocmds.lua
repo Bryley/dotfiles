84000fe983875ad506cdfd8771b07a80a8ba4b2d
Bryley <bryley.manninghayter@ladbrokes.com.au>
2023-02-24 16:10:24 +1000

    Small changes

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,4 +1,4 @@
 
 
--- Set textwidth and wrap for markdown and norg files
-vim.cmd([[autocmd FileType markdown,norg,txt setlocal textwidth=80 wrap]])
+-- Set textwidth for markdown and norg files
+vim.cmd([[autocmd FileType markdown,norg,txt setlocal textwidth=80]])
