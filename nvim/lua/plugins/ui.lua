aa3d28ef2e02dbd1115db739c770099140da4078
Bryley <bryley.manninghayter@ladbrokes.com.au>
2023-02-28 17:00:16 +1000

    Lualine plugin added filepath instead of just the name

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -70,7 +70,24 @@ return {
 	{},
 	{
 		"nvim-lualine/lualine.nvim",
-		config = true,
+		opts = {
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { { "filename", path = 1 } },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { { "filename", path = 1 } },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+		},
 	},
 	{
 		-- Adds indent guides to the code
